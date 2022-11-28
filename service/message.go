c121a35dfb0c945bee3799d9ef1f88e24f22b089
XiaoTe33 <1253810338@qq.com>
2022-11-28 21:24:54 +0800

    feat(service): 新增文本与用户名密码的入参限制

--- service/message.go
+++ service/message.go
@@ -1,8 +1,19 @@
 package service
 
+import (
+	"message-board/util"
+	"strings"
+)
+
 func TextIsValid(text string) bool { //文本长度小于80
-	if len([]rune(text)) <= 80 {
-		return true
+	if len([]rune(text)) > 80 {
+		return false
 	}
-	return false
+	for _, str := range util.InvalidWords { //包含无效词汇
+		if strings.Contains(text, str) {
+			return false
+		}
+	}
+	return true
+
 }
--- service/user.go
+++ service/user.go
@@ -1,15 +1,45 @@
 package service
 
+import (
+	"fmt"
+	"message-board/util"
+	"regexp"
+	"strings"
+)
+
 func UsernameIsValid(username string) bool { //名字长度小于20
-	if len([]rune(username)) <= 20 {
-		return true
+	if len([]rune(username)) > 20 {
+		return false
 	}
-	return false
+
+	reg := regexp.MustCompile("[^A-Za-z0-9]") //不能有其他字符
+	if reg == nil {
+		fmt.Println("reg err")
+	}
+	allString := reg.FindAllString(username, -1)
+	if allString[0] != "" {
+		return false
+	}
+
+	for _, str := range util.InvalidWords { //不能包含无效词汇
+		if strings.Contains(username, str) {
+			return false
+		}
+	}
+	return true
 }
 
 func PasswordIsValid(password string) bool { //密码长度小于20
-	if len([]rune(password)) <= 20 {
-		return true
+	if len([]rune(password)) > 20 {
+		return false
 	}
-	return false
+	reg := regexp.MustCompile("[^A-Za-z0-9]") //不能有其他字符
+	if reg == nil {
+		fmt.Println("reg err")
+	}
+	allString := reg.FindAllString(password, -1)
+	if allString[0] != "" {
+		return false
+	}
+	return true
 }
