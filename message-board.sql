fe8406570a97ddba0961b820e9d9467bf83eca23
XiaoTe33 <1253810338@qq.com>
2022-11-30 22:45:43 +0800

    docs: 数据库生成文件改动

--- message-board.sql
+++ message-board.sql
@@ -18,14 +18,31 @@ create table messages
 );
 
 insert into userdata(uid, username, password)
-values (1, 'xiaote', '1');
+values (1, 'xiaote11', '123456');
 insert into userdata(uid, username, password)
-values (null, 'xiaote33', '1');
+values (null, 'xiaote22', '123456');
+insert into userdata(uid, username, password)
+values (null, 'xiaote33', '123456');
+insert into userdata(uid, username, password)
+values (null, 'xiaote44', '123456');
+insert into userdata(uid, username, password)
+values (null, 'xiaote55', '123456');
+insert into userdata(uid, username, password)
+values (null, 'xiaote66', '123456');
+insert into userdata(uid, username, password)
+values (null, 'xiaote77', '123456');
 
 insert into messages(mid, sender, receiver, time, text)
-values (1, 'xiaote', 'xiaote33', '09:32:33', 'I love U');
+values (1, 'xiaote11', 'xiaote33', '09:32:33', 'I love U');
 insert into messages(mid, sender, receiver, time, text)
-values (2, 'xiaote33', 'xiaote', '09:32:34', 'I love U, too');
+values (null, 'xiaote33', 'xiaote11', '09:32:34', 'I love U, too');
+insert into messages(mid, sender, receiver, time, text)
+values (null, 'xiaote11', 'xiaote33', '09:32:35', 'really');
+insert into messages(mid, sender, receiver, time, text)
+values (null, 'xiaote33', 'xiaote11', '09:32:36', 'yes');
+insert into messages(mid, sender, receiver, time, text)
+values (null, 'xiaote33', 'xiaote11', '09:32:37', 'I truly love you');
+
 
 create table comments
 (
@@ -38,27 +55,29 @@ create table comments
     rid     int         default '0'
 );
 insert into comments(mid, cid, sender, time, text)
-values (1, 1, 'xiaote33', '10:14:22', 'I love U, too');
+values (1, 1, 'xiaote22', '10:14:22', '99');
 insert into comments(mid, cid, sender, time, text)
-values (1, null, 'xiaote33', '10:14:23', 'I love U, too');
+values (1, null, 'xiaote44', '10:14:23', '9999');
 insert into comments(mid, cid, sender, time, text)
-values (1, null, 'xiaote33', '10:14:24', 'I love U, too');
+values (1, null, 'xiaote55', '10:14:24', '99999');
 insert into comments(mid, cid, sender, time, text)
-values (1, null, 'xiaote33', '10:14:25', 'I love U, too');
+values (1, null, 'xiaote66', '10:14:25', '999999');
 insert into comments(mid, cid, sender, time, text)
-values (1, null, 'xiaote33', '10:14:26', 'I love U, too');
+values (1, null, 'xiaote77', '10:14:26', '9999999');
+
+
 
 insert into comments(mid, sender, time, text, rid)
-values (1,'xiaote','10:14:27','resp1',1);
+values (1,'xiaote','10:14:27','6',1);
 insert into comments(mid, sender, time, text, rid)
-values (1,'xiaote33','10:14:28','resp3',6);
+values (1,'xiaote33','10:14:28','666',6);
 insert into comments(mid, sender, time, text, rid)
-values (1,'xiaote33','10:14:29','resp4',6);
+values (1,'xiaote33','10:14:29','666',6);
 insert into comments(mid, sender, time, text, rid)
-values (1,'xiaote','10:14:30','resp5',8);
+values (1,'xiaote','10:14:30','6666',8);
 insert into comments(mid, sender, time, text, rid)
-values (1,'xiaote','10:14:31','resp6',8);
+values (1,'xiaote','10:14:31','6666',8);
 insert into comments(mid, sender, time, text, rid)
-values (1,'xiaote33','10:14:32','resp7',10);
+values (1,'xiaote33','10:14:32','666666',10);
 insert into comments(mid, sender, time, text, rid)
-values (1,'xiaote33','10:14:33','resp8',10);
+values (1,'xiaote33','10:14:33','666666',10);
